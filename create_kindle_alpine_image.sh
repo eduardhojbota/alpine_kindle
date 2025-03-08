31632710e0f08cb61fc48980839c439717d76bcb
Somethong <121753192+Entersjkhdfkjdhfksjf@users.noreply.github.com>
2025-03-08 21:07:48 +0530

    Update create_kindle_alpine_image.sh

--- create_kindle_alpine_image.sh
+++ create_kindle_alpine_image.sh
@@ -17,7 +17,7 @@ apk update
 apk upgrade
 cat /etc/alpine-release
 apk add xorg-server-xephyr xwininfo xdotool xinput dbus-x11 sudo bash nano git
-apk add desktop-file-utils gtk-engines consolekit gtk-murrine-engine thunar marco gnome-themes-extra
+apk add desktop-file-utils gtk-engines consolekit gtk-murrine-engine thunar marco gnome-themes-extra xfce4-battery-plugin
 apk add xfce4
 apk add \$(apk search -q ttf- | grep -v '\-doc')
 apk add onboard chromium
